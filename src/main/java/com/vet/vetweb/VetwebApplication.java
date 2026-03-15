48e2b81326d1964f8710ced60fa94e40898debdb
0Dyzzy <kev.castilloc@outlook.com>
2026-03-15 20:54:56 -0300

    feat: first commit

---
+++ src/main/java/com/vet/vetweb/VetwebApplication.java
@@ -0,0 +1,13 @@
+package com.vet.vetweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetwebApplication.class, args);
+	}
+
+}
